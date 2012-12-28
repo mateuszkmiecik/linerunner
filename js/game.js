2c1f62bf7c61ab21cac9f484615c095189b633f5
Mateusz Kmiecik <mk.chomik@gmail.com>
2012-12-28 13:53:07 +0100

    added rolling state

--- js/game.js
+++ js/game.js
@@ -7,13 +7,19 @@ window.onload = function(){
 		// base canvas elements
 		var canvas, ctx, W, H;
 
-		var playing = false;
+		// main states of game
+		var playing = false,
+			dead = false;
 
-		// active sprite for stickman moving
-		var active = 1;
+		// stickman moving
+		var active = 1, // active sprite
+			jumping = false,
+			rolling = false; 
 
-		// start animation variables
-		var startAnimation = false,
+
+		// start button and animation variables
+		var startButton,
+			startAnimation = false,
 			startOffset = 215,
 			startAnimationMultiplier = 1;
 
@@ -23,6 +29,8 @@ window.onload = function(){
 			sprites[i] = new Image();
 			sprites[i].src = 'sprites/sprite'+i+'.png';
 		}
+		sprites[7] = new Image();
+		sprites[7].src="sprites/rolling.png";
 
 		// init
 		self.init = function(){
@@ -34,6 +42,8 @@ window.onload = function(){
 
 			// start button listener
 			startButton.addEventListener('click', events.startClick, false);
+			document.addEventListener('keydown', events.keydown, false);
+			document.addEventListener('keyup', events.keyup, false);
 
 			// basement
 			ctx.fillRect(0,200,W,100);
@@ -43,17 +53,34 @@ window.onload = function(){
 		}
 
 		function draw(){
-			ctx.clearRect(0,0,W,H-100);
-			ctx.drawImage(sprites[active],20+startOffset,167);
+			ctx.clearRect(0,0,W,H-100); // clear
+			ctx.save();
+
+			// stickman
+			if(rolling){
+
+				ctx.translate(20+20,177+25/2);
+				ctx.rotate(Math.PI/6*active);
+				ctx.drawImage(sprites[7],-25/2,-25/2);
+				ctx.restore();
+
+			}else{
+				ctx.drawImage(sprites[active],20+startOffset,168);
+			}
+
+			// next frame
 			update();
 		}
 
 		function update(){
+			// active sprite of stickman
 			active += 1;
 			if(active>6){
 				active = active%6;
 			}
 
+
+			// startAnimation
 			if(startAnimation){
 				if(startOffset-20 > 0){
 					startOffset -= startAnimationMultiplier*1,5;
@@ -73,12 +100,11 @@ window.onload = function(){
 			}
 
 			// debugging
-			self.sa = startAnimation;
 			self.pa = playing;
 		}
 
 		// startButton
-		var startButton = (function(){
+		startButton = (function(){
 			var container = document.getElementById('container');
 			var startBtn = document.createElement('button');
 			startBtn.setAttribute('class', 'start');
@@ -89,11 +115,38 @@ window.onload = function(){
 			return startBtn;
 		}());
 
+
+		// events 
 		var events = {
 			startClick : function(){
 				startAnimation = true;
+			},
+			keydown : function(event){
+				switch(event.keyCode){
+					case 38: // arrow up
+						if(playing){
+							event.preventDefault();
+							console.log('skaczemy!');
+						}
+					break;
+					case 40: // arrow down
+						if(playing){
+							event.preventDefault();
+							rolling = true;
+						}
+					break;
+				}
+			},
+			keyup : function(event){
+				switch(event.keyCode){
+					case 40:
+						if(rolling) rolling = false;
+					break;
+				}
 			}
 		}
+
+
 		////////////
 		return self;
 
